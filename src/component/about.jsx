8192e6022754824fe8a7941299ac8e1ea0566f7f
RyanMustofa <rm281003@gmail.com>
2020-04-27 16:02:14 +0700

    finall

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -8,7 +8,7 @@ const About = () => {
                 <div class="row profile">
                     <div class="col-md-3">
                         <div class="profile-sidebar">
-                            <div class="profile-userpic">
+                            <div class="profile-userpic mx-auto text-center">
                                 <img
                                     src="https://gravatar.com/avatar/31b64e4876d603ce78e04102c67d6144?s=80&d=https://codepen.io/assets/avatars/user-avatar-80x80-bdcd44a3bfb9a5fd01eb8b86f9e033fa1a9897c3a15b33adfc2649a002dab1b6.png"
                                     class="img-responsive"
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import '@material/react-material-icon/dist/material-icon.css';
 
 ReactDOM.render(
     <BrowserRouter>
